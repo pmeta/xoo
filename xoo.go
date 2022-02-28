a88faffab888153d7281ff4caca7be31ac7edf0b
jianxin.lu <jianxin.lu@woqutech.com>
2022-02-28 21:34:13 +0800

    增加前台展示页模板

--- xoo.go
+++ xoo.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"net/http"
 	"runtime"
+
+	"github.com/gin-gonic/gin"
 
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
@@ -17,13 +21,27 @@ var (
 	branch   string
 	platform = fmt.Sprintf("%s/%s %s", runtime.GOOS, runtime.GOARCH, runtime.Version())
 
-	webListenAddr = kingpin.Flag("meta.listen-addr", "监听地址").Default(":80").String()
+	webListenAddr = kingpin.Flag("meta.listen-addr", "监听地址").Default(":8080").String()
+	//go:embed xoo
+	uiIndex embed.FS
 )
 
 func init() {
-
+	gin.DisableConsoleColor()
+	gin.SetMode(gin.DebugMode)
+	kingpin.Parse()
 }
 
 func main() {
-
+	router := gin.Default()
+	router.StaticFS("/static", http.FS(uiIndex))
+	router.Any("", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.html", nil)
+	})
+	if err := router.Run(*webListenAddr); err != nil {
+		return
+	}
+	//if err := http.ListenAndServe(*webListenAddr, http.FileServer(http.FS(uiIndex))); err != nil {
+	//	return
+	//}
 }
