945702977dd4a519521d5d0915a9777f8f8fab8a
jeyrce <jeyrce@gmail.com>
2022-03-08 23:45:29 +0800

    完成简易前端页面

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/skrbox/xoo
 go 1.17
 
 require (
+	github.com/gin-gonic/gin v1.7.7
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
+
+require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.7.7 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
@@ -21,6 +25,5 @@ require (
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- xoo.go
+++ xoo.go
@@ -3,7 +3,10 @@ package main
 import (
 	"embed"
 	"fmt"
+	"html/template"
 	"net/http"
+	"os"
+	"path"
 	"runtime"
 
 	"github.com/gin-gonic/gin"
@@ -22,8 +25,11 @@ var (
 	platform = fmt.Sprintf("%s/%s %s", runtime.GOOS, runtime.GOARCH, runtime.Version())
 
 	webListenAddr = kingpin.Flag("meta.listen-addr", "监听地址").Default(":8080").String()
-	//go:embed xoo
-	uiIndex embed.FS
+	webPrefix     = kingpin.Flag("meta.web-prefix", "路由地址前缀").Default("/xoo/").String()
+	//go:embed static
+	static embed.FS
+	//go:embed html/*.html
+	index embed.FS
 )
 
 func init() {
@@ -34,14 +40,16 @@ func init() {
 
 func main() {
 	router := gin.Default()
-	router.StaticFS("/static", http.FS(uiIndex))
-	router.Any("", func(context *gin.Context) {
-		context.HTML(http.StatusOK, "index.html", nil)
+	html, _ := template.ParseFS(index, "html/*.html")
+	router.SetHTMLTemplate(html)
+	router.Any(path.Join(*webPrefix, ""), func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"staticRoot": path.Join(*webPrefix, "static"),
+			"title":      "秀哦工作室",
+		})
 	})
+	router.StaticFS(path.Join(*webPrefix, ""), http.FS(static))
 	if err := router.Run(*webListenAddr); err != nil {
-		return
+		os.Exit(1)
 	}
-	//if err := http.ListenAndServe(*webListenAddr, http.FileServer(http.FS(uiIndex))); err != nil {
-	//	return
-	//}
 }
